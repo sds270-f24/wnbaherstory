fdbe2ed04dcae6cfd73b8fa07c2adde4952968dc
esylvester04 <esylvester@smith.edu>
2024-11-26 11:14:24 -0500

    fixed merge conflicts

--- data-raw/hexsticker.R
+++ data-raw/hexsticker.R
@@ -12,18 +12,10 @@ sticker(
   s_height = 0.9,
   s_x = 1,
   s_y = 0.75,
-<<<<<<< HEAD
   p_size = 15,
   h_fill = '#fa4d00',
   h_color = 'black',
   h_size = 1.5,
   p_color = 'white'
 ) %>% print()
-=======
-  p_size = 12,
-  h_fill = '#fa4d00',
-  h_color = '#fa4d00',
-  h_size = 1.5,
-  p_color = 'white'
-) %>% print()
->>>>>>> 4fbad1bac1cd608a76f79964a7097513124ffa86
+
