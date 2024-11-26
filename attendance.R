31e65070a98270aef8fda9d7a2f6f86f0c3c9c6b
aabdulina <aabdulina@smith.edu>
2024-11-26 11:45:15 -0500

    attendance function

--- attendance.R
+++ attendance.R
@@ -1,33 +1,32 @@
 #' Function graphs a WNBA team attendance record over the years
 #' @author Alina Abdulina
-#' @param team string conatining the name of the team
+#' @param team string containing the name of the team
 #' @return graph
-#' @import ggplot2
-#' @import tidyverse
-#' @source https://www.acrossthetimeline.com/wnba/data.html# 
+#' @source <https://www.acrossthetimeline.com/wnba/data.html#>
+#' @export
 #' @examples
 #' plot_wnba_attendance("Los Angeles Sparks")
 
 plot_wnba_attendance <- function(team) {
   
-  # Step 1: Filter the dataset for the chosen team
+  # Step 1: Filter the data set for the chosen team
   team_data <- All_Game_Attendance |>
-    filter(home_team == team)
+    dplyr::filter(`Home Team` == team)
   
   # Step 2: Calculate the average attendance for each year
   avg_attendance <- team_data |>
-    group_by(Year) |>
-    summarise(Average_Attendance = mean(Attendance, na.rm = TRUE))
+    dplyr::group_by(Year) |>
+    dplyr::summarise(Average_Attendance = mean(Attendance, na.rm = TRUE))
   
   # Step 3: Plot the average attendance for the specified team over time
-  ggplot(data = avg_attendance, 
-         aes(x = Year, 
-             y = Average_Attendance)) +
-    geom_line(color = "purple1") +
-    labs(title = paste("Attendance Trends for", team),
-         x = "Year",
-         y = "Average Attendance Per Year") +
-    theme_minimal()
+  ggplot2::ggplot(data = avg_attendance, 
+                  ggplot2::aes(x = Year,
+                               y = Average_Attendance)) +
+    ggplot2::geom_line(color = "purple1") +
+    ggplot2::labs(title = paste("Attendance Trends for", team),
+                  x = "Year",
+                  y = "Average Attendance Per Year") +
+    ggplot2::theme_minimal()
   
 }
 
