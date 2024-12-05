ba1e08216a1ce6b6fbbd954259a3f49c8062c73f
aabdulina <aabdulina@smith.edu>
2024-12-05 11:35:44 -0500

    changes

---
+++ R/Data.R
@@ -0,0 +1,24 @@
+#' All Game Attendance Data
+#'
+#' This data set contains information about the attendance of all games in a certain season or series.
+#' It includes columns such as game date, attendance numbers, and other related statistics.
+
+#' @docType data
+#' @usage data(All_Game_Attendance)
+#' @format A data frame with 6214 rows and 13 variables:
+#' \describe{
+#'    \item{Year}{Integer. The year the teams played.}
+#'    \item{Day of Week}{Factor. The day of the week the teams played.}
+#'    \item{Month}{Factor. The month the teams played.}
+#'    \item{Day}{Integer. Day of the week the teams played.}
+#'    \item{Time(ET)}{The time the game was played at.}
+#'    \item{Game Type}{Factor. The game is classified either as regular season, Seminfinals, Finals, Eastern Conference Semifinals etc.}
+#'    \item{Home Team}{Fcator. Name of the home team.}
+#'    \item{Away Team}{Factor. Name of the away team.}
+#'    \item{Arena}{Factor. Name of the arena the game was played at.}
+#'    \item{City}{Factor. Name of the city the game was played at.}
+#'    \item{State}{Factor. Name of the State the game was played at.}
+#'    \item{Attendance}{Integer. Number of people who attended the game.}
+#' }
+#' @source <https://www.acrossthetimeline.com/wnba/data.html#>
+"All_Game_Attendance"
--- data-raw/All-Game-Attendance-csv.R
+++ data-raw/All-Game-Attendance-csv.R
@@ -1,29 +1,4 @@
 ## code to prepare `All Game Attendance.csv` dataset goes here
-#' All Game Attendance Data
-#'
-#' This data set contains information about the attendance of all games in a certain season or series.
-#' It includes columns such as game date, attendance numbers, and other related statistics.
-#' @import readr
-#' @docType data
-#' @usage data(All_Game_Attendance)
-#' @format A data frame with 6214 rows and 13 variables:
-#' \describe{
-#'    \item{Year}{Integer. The year the teams played.}
-#'    \item{Day of Week}{Factor. The day of the week the teams played.}
-#'    \item{Month}{Factor. The month the teams played.}
-#'    \item{Day}{Integer. Day of the week the teams played.}
-#'    \item{Time(ET)}{The time the game was played at.}
-#'    \item{Game Type}{Factor. The game is classified either as regular season, Seminfinals, Finals, Eastern Conference Semifinals etc.}
-#'    \item{Home Team}{Fcator. Name of the home team.}
-#'    \item{Away Team}{Factor. Name of the away team.}
-#'    \item{Arena}{Factor. Name of the arena the game was played at.}
-#'    \item{City}{Factor. Name of the city the game was played at.}
-#'    \item{State}{Factor. Name of the State the game was played at.}
-#'    \item{Attendance}{Integer. Number of people who attended the game.}
-#' }
-#' @source <https://www.acrossthetimeline.com/wnba/data.html#>
-"All_Game_Attendance"
-
 
 All_Game_Attendance <- readr::read_csv("All Game Attendance.csv")
 
